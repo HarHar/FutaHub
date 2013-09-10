0e1372858bb1eed6979a71a0b63209f5ceef481b
HarHar <harhar-captain@live.com>
2013-09-10 17:15:29 -0300

    Add "count" to the field whitelist, fixing numbering bug

--- main.py
+++ main.py
@@ -352,6 +352,7 @@ else:
 			sanitized = {'items': []}
 			sanitized['name'] = jsondb['name'][:128]
 			sanitized['description'] = jsondb['description'][:256]
+			sanitized['count'] = int(str(jsondb['count'])[:128])
 			for i, entry in enumerate(jsondb['items']):
 				e = jsondb['items'][i]
 				sanitized['items'].append({'status': e['status'][0], 'hash': e['hash'][:128], 'name': e['name'][:128], 'obs': e['obs'][:128], 'lastwatched': str(e['lastwatched'])[:64], 'genre': e['genre'][:256], 'aid': e['aid'], 'type': e['type'][:12], 'id': e['id']})
